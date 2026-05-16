ee8e6e324848c7b434140fc00d9f07d6de4e89ae
Vehrah <chinecheremvera8@gmail.com>
2026-05-16 16:15:42 +0100

    Refactor mongoose connection to async function
    Refactor database connection to use async/await and improve error handling.

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
-function connect(uri, opts = {}) {
-  return mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, ...opts });
+const dbConnect = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Database connected: ${connect.connection.host}, ${connect.connection.name}`)
 }
-
-module.exports = { connect, mongoose };
+    catch (error) {
+        console.log(error)
+        process.exit(1)
+    }  
+}
+module.exports = dbConnect
