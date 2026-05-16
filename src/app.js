9efb6e646f3bc42d5d36c60eac2366981cf43ea1
Vehrah <chinecheremvera8@gmail.com>
2026-05-16 16:19:07 +0100

    Initialize dotenv and configure DNS servers
    Set up environment configuration and DNS servers.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+const dns = require("node:dns")
+dns.setServers(['1.1.1.1', '8.8.8.8'])
+const dbConnect = require("./config/db")
+
+dbConnect()
 
 app.use(express.json());
+const authRoutes = require("./routes/auth.routes");
+
+app.use("/api/auth", authRoutes);
 
 app.get('/health', (req, res) => res.json({ ok: true }));
 
